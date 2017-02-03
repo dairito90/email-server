01e151333aeadf32608f9796a7f4d09608645a38
Dairon Rodriguez <drodriguez@fvi.edu>
2017-02-02 20:55:56 -0500

    add send email-form

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var webApp = express();
 var bodyParser = require('body-parser');
 var nodemailer = require('nodemailer');
+var fs = require('fs');
+var cors = require('cors');
 var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -14,7 +16,7 @@ var transporter = nodemailer.createTransport({
 
 
 
-
+webApp.use(cors());
 webApp.use(bodyParser.json());
 webApp.use(bodyParser.urlencoded({
     extended: true
@@ -28,7 +30,7 @@ webApp.post('/email', function(req, res) {
     var postedData = req.body;
     transporter.sendMail({
             from: 'roddairon@gmail.com',
-            to: 'vmoreno@fvi.edu',
+            to: 'drodriguez@fvi.edu',
             subject: 'this is a test',
             html: postedData.msg
         },
@@ -43,6 +45,31 @@ webApp.post('/email', function(req, res) {
 
 });
 
+webApp.post('/send-resume', function(req, res) {
+    fs.readFile('./resume.html', function(err, contents) {
+        if (err) {
+            console.log(err);
+            res, sendStatus(500);
+            return;
+        }
+        transporter.sendMail({
+                from: 'roddairon@gmail.com',
+                to: 'drodriguez@fvi.edu',
+                subject: 'this is a test send email',
+                html: contents
+            },
+            function(err, info) {
+                if (err) {
+                    console.log(JSON.stringify(err));
+                    return res.sendStatus(500);
+                }
+                console.log(JSON.stringify(info));
+                res.json(info);
+            });
+    });
+
+
+});
 
 webApp.listen(8967);
 console.log('server listening');
